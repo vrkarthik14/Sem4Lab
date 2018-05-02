5d6dc31c5b62ef7467e6b9b2522ac3e4d036a238
V R KARTHIK <vrkarthik14@gmail.com>
2018-05-02 23:35:10 +0530

    Updated DFS

--- DFS.c
+++ DFS.c
@@ -1,36 +1,42 @@
-#include<stdio.h>
- 
-void DFS(int);
-int G[10][10],visited[10],n;    //n is no of vertices and graph is sorted in array G[10][10]
- 
+#include <stdio.h>
+int count =0;
+int dfs(int [20][20],int,int[20],int);
 void main()
 {
-    int i,j;
-    printf("Enter number of vertices:");
-   
-    scanf("%d",&n);
- 
-    //read the adjecency matrix
-    printf("\nEnter adjecency matrix of the graph:");
-   
-    for(i=0;i<n;i++)
-       for(j=0;j<n;j++)
-            scanf("%d",&G[i][j]);
- 
-    //visited is initialized to zero
-   for(i=0;i<n;i++)
-        visited[i]=0;
- 
-    DFS(0);
+        int n,a[20][20],i,j,visited[20],source,visitedorder[20];
+        printf("Enter the no of vertices\n");
+        scanf("%d",&n);
+        printf("Enter the adjacency matrix\n");
+        for(i=1; i<=n; i++)
+                for(j=1; j<=n; j++)
+                        scanf("%d",&a[i][j]);
+        for(i=1; i<=n; i++)
+                visited[i]=0;
+        printf("Enter source node\n");
+        scanf("%d",&source);
+        for(i=1; i<=n; i++)
+                if(visited[i]==0)
+                        dfs(a,n,visited,i);
+        for(i=1; i<=n; i++)
+        {
+                visitedorder[visited[i]]=i;
+        }
+        printf("DFS traversal= \n");
+        for(i=1; i<=n; i++)
+        {
+                printf("%c \t",visitedorder[i]+64);
+        }
 }
- 
-void DFS(int i)
+int dfs(int a[20][20],int n,int visited[20],int source)
 {
-    int j;
-    printf("\n%d",i);
-    visited[i]=1;
-    
-    for(j=0;j<n;j++)
-       if(!visited[j]&&G[i][j]==1)
-            DFS(j);
+        count++;
+        int v;
+        visited[source]=count;
+        for(v=1; v<=n; v++)
+        {
+                if(a[source][v]==1&&visited[v]==0)
+                {
+                        dfs(a,n,visited,v);
+                }
+        }
 }
